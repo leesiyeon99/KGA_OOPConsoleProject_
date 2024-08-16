a70f5ea8a1bc8f6bd12b5a0ece68c8062a92a231
leesiyeon99 <qsqs9090@naver.com>
2024-08-16 15:22:12 +0900

    [Edit] hammer
    망치 주웠을 때 아이템칸이 차있으면 주울 수 없음

--- RPG/GameObjects/HammerObject.cs
+++ RPG/GameObjects/HammerObject.cs
@@ -17,11 +17,23 @@ namespace RPG.GameObjects
 
         public override void Interaction(Player player)
         {
-            Inventory.AddItem(LastScene.Item);
-            removeWhenInteract = true;
-            Console.SetCursorPosition(0, Console.WindowHeight - 15);
-            Console.WriteLine("망치를 주웠습니다. 인벤토리에 보관하였습니다.");
-            Thread.Sleep(1000);
+            if (Inventory.AddItem(LastScene.Item))
+            {
+                removeWhenInteract = true;
+                Console.SetCursorPosition(0, Console.WindowHeight - 15);
+                Console.WriteLine("망치를 주웠습니다. 인벤토리에 보관하였습니다.");
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                Player.playerPos.x = 13;
+                Player.playerPos.y = 3;
+                removeWhenInteract = false;
+                Console.SetCursorPosition(0, Console.WindowHeight - 15);
+                Console.WriteLine("인벤토리가 가득 찼습니다. 망치를 주울 수 없습니다.");
+                Console.WriteLine("인벤토리에 있는 아이템을 사용하거나 상점에 판매해주세요.");
+                Thread.Sleep(2000);
+            }
         }
     }
 }
